fb14bf293a7c03608d676db45cd22ee463ac61b2
yopipi <ysk0420kbt@gmail.com>
2021-08-08 19:10:28 +0900

    first commit

---
+++ blogpost/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+CATEGORY =(('business','ビジネス'),('life','生活'),('other','その他'))
+
+class BlogModel(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    postdate = models.DateField(auto_now_add=True)
+    category = models.CharField(
+        max_length=50,
+        choices=CATEGORY
+    )
+    def __str__(self):
+        return self.title
+
